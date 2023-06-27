1de3a2b61fef1ec8eed90e485c2674a3a2b7067a
sehyeogi360 <lsh27572757@gmail.com>
2023-06-27 12:08:48 +0900

    [임세혁] - 비오의 autowired를지우니 잘된다 왜그럴까

--- src/main/java/com/marondal/welibrary/user/dao/UserDAO.java
+++ src/main/java/com/marondal/welibrary/user/dao/UserDAO.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDAO {
 	
-	//public int insertUser(
-	//		@Param("loginId") String loginId
-	//		, @Param("password") String password
-	///		, @Param("name") String name
-	//		, @Param("email") String email
-	//		, @Param("phoneNumber") String phoneNumber
-	//		
-	//		);
+	public int insertUser(
+			@Param("loginId") String loginId
+			, @Param("password") String password
+			, @Param("name") String name
+			, @Param("email") String email
+			, @Param("phoneNumber") String phoneNumber
+		
+			);
 
 }
