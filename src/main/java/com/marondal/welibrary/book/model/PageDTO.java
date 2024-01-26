eca9a76668d8ae5eb601d5bdc42572d7c80c5b08
sehyeogi360 <lsh27572757@gmail.com>
2024-01-26 23:15:54 +0900

    [임세혁] -변수 구현중

--- src/main/java/com/marondal/welibrary/book/model/PageDTO.java
+++ src/main/java/com/marondal/welibrary/book/model/PageDTO.java
@@ -1,5 +1,11 @@
 package com.marondal.welibrary.book.model;
 
 public class PageDTO {
-
+	private int pageNum_cnt;//한번에 표시할 페이징 번호의 갯수
+	
+	private int startPageNum;//표시되는 페이지 번호 중 첫번째 번호
+	
+	private int endPageNum;//표시되는 페이지 번호 중 마지막 번호
+	
+	private int total;//전체 게시물 수 
 }
