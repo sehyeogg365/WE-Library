33edc05bfeea48b47787d274dc4e1be352dd121b
sehyeogi360 <lsh27572757@gmail.com>
2024-01-29 21:30:50 +0900

    [임세혁] -페이지dto 참고사안

--- src/main/java/com/marondal/welibrary/book/model/PageDTO.java
+++ src/main/java/com/marondal/welibrary/book/model/PageDTO.java
@@ -8,4 +8,12 @@ public class PageDTO {
 	private int endPageNum;//표시되는 페이지 번호 중 마지막 번호
 	
 	private int total;//전체 게시물 수 
+	/**
+	    * 페이징 계산
+	    * @param currPage 현재페이지
+	    * @param totalPage 총페이지
+	    * @param firstPage 첫페이지
+	    * @param lastPage 마지막페이지
+	    * @param totalData 총 DATA
+	    */
 }
